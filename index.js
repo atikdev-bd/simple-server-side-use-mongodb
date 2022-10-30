0596022820027a06c9944dd5f74425c57e244be5
atikdev-bd <atikdev.bd@gmail.com>
2022-10-30 17:46:04 +0600

    mongodb connected done

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const users = [
   { id: 1, name: "Mizanur Rahman", email: "mizan@gmail.com" },
@@ -22,33 +21,41 @@ app.get("/", (req, res) => {
 
 /// password : Jv1yXHde8hO89h2d
 
-
-
-const uri = "mongodb+srv://dbuser2:Jv1yXHde8hO89h2d@cluster0.h7epoo8.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const uri =
+  "mongodb+srv://dbuser2:Jv1yXHde8hO89h2d@cluster0.h7epoo8.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+const run = async () => {
+  try {
+    const userCollection = client.db("simpleNode").collection("users");
+    const users = { name: "Samiya", email: "sami@gmail.com" };
 
+    const result = await userCollection.insertOne(users);
+    console.log(result);
+  } finally {
+
+
+
+  }
+};
+run().catch(error => console.log(error))
 
 app.post("/users", (req, res) => {
-
-    
-    if(req.query.name){
-        const search = req.query.name
-        const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >=0)
-        res.send(filtered)
-    }else{
-        res.send(user)
-    }
-  const user = req.body
-  user.id = users.length +1
-  users.push(user)
- 
-  
- 
+  if (req.query.name) {
+    const search = req.query.name;
+    const filtered = users.filter(
+      (usr) => usr.name.toLowerCase().indexOf(search) >= 0
+    );
+    res.send(filtered);
+  } else {
+    res.send(user);
+  }
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
 });
 
 app.get("/users", (req, res) => {
